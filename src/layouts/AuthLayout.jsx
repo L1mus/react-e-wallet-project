8f46d91e4023c1d714bbf96f44b1e94f92b9e122
L1mus <ali.firefox@yahoo.com>
2026-04-05 23:00:02 +0700

    feat : add component Layout Auth

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,49 @@
+export const AuthLayout = ({ children, title, subtitle, imagePath }) => {
+  return (
+    <div className="relative h-screen w-full bg-primary font-sans overflow-hidden flex">
+      <div className="relative z-10 w-full md:w-1/2 h-full bg-white md:rounded-r-[40px] shadow-[10px_0_30px_rgba(0,0,0,0.15)] flex flex-col justify-center items-center px-8 lg:px-12 overflow-hidden">
+        <div className="w-full max-w-140.5">
+          <div className="mb-5 md:mt-0">
+            <div className="flex items-center gap-2 mb-4 lg:mb-5">
+              <img
+                src="/assets/icons/Money Wallet.svg"
+                alt="Logo"
+                className="w-8 h-8 object-contain"
+              />
+              <h1 className="text-primary font-bold text-xl tracking-wide">
+                E-Wallet
+              </h1>
+            </div>
+
+            <h1 className="text-2xl lg:text-3xl font-medium text-grey mb-1 leading-tight">
+              {title}
+            </h1>
+            <p className="text-sm lg:text-base font-normal text-grey leading-snug">
+              {subtitle}
+            </p>
+          </div>
+
+          <div className="w-full">{children}</div>
+        </div>
+      </div>
+
+      <div className="hidden md:flex absolute top-0 right-0 w-1/2 h-full items-center justify-center pointer-events-none bg-primary overflow-hidden">
+        <div className="relative z-20 flex items-center justify-center w-full h-full">
+          <div
+            className="absolute w-125 h-125 rounded-full z-0 translate-y-10"
+            style={{
+              background: "radial-gradient(circle, #7096FF 0%, #3868FD 87%)",
+              opacity: 0.8,
+            }}
+          />
+          <div className="absolute w-100 h-100 bg-white/10 rounded-full blur-3xl z-1 translate-y-20 pointer-events-none" />
+          <img
+            src={imagePath}
+            alt="Illustration"
+            className="relative z-10 w-4/5 max-w-125 object-contain hover:scale-105 transition-transform duration-700 drop-shadow-2xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
