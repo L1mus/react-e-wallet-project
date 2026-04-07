2d6ee0dbc589431a2780845c9e3f148754d4cc7d
l1mus <alinesezcore@gmail.com>
2026-04-07 23:26:06 +0700

    feat : Add Route path for Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Register } from "./pages/auth/Register";
 import { ForgotPassword } from "./pages/auth/ForgotPassword";
 import { EnterPin } from "./pages/auth/EnterPin";
 import { CreatePin } from "./pages/auth/CreatePin";
+import { Dashboard } from "./pages/dashboard/Dashboard";
+import { DashboardLayout } from "./layouts/DashboardLayout";
 
 function App() {
   return (
@@ -15,6 +17,21 @@ function App() {
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/enter-pin" element={<EnterPin />} />
       <Route path="/create-pin" element={<CreatePin />} />
+      <Route path="/dashboard" element={<DashboardLayout />}>
+        <Route index element={<Dashboard />} />
+        {/* <Route path="transfer" element={<TransferPage />} />
+        <Route path="history" element={<HistoryPage />} />
+        <Route
+          path="topup"
+          element={<div className="text-2xl font-bold">Ini Halaman Top Up</div>}
+        />
+        <Route
+          path="profile"
+          element={
+            <div className="text-2xl font-bold">Ini Halaman Profile</div>
+          }
+        /> */}
+      </Route>
     </Routes>
   );
 }
