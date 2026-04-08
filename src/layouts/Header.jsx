609ecd28c098357da9325e8d077895c2dadfca17
l1mus <alinesezcore@gmail.com>
2026-04-08 11:50:05 +0700

    refactor: make condition for login state and  logout state

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router";
 import { Search, ShoppingCart, ChevronDown } from "lucide-react";
 import { Button } from "../components/ui/Button";
 import iconMoneyWallet from "../assets/icons/Money-Wallet.svg";
+import { getSession, clearSession } from "../utils/storage";
+import { useState } from "react";
 
 /**
  * user
@@ -15,10 +17,12 @@ import iconMoneyWallet from "../assets/icons/Money-Wallet.svg";
  * @returns
  */
 
-export const Header = ({ variant = "landing", user }) => {
-  const profileName = user?.name || "Ghaluh Wizard";
+export const Header = ({ variant = "landing" }) => {
+  const [, setIsLogin] = useState(true);
+  const data = getSession();
+  const profileName = data?.name || "Ghaluh Wizard";
   const profilePic =
-    user?.profilePicture ||
+    data?.profilePicture ||
     `https://ui-avatars.com/api/?name=Ghaluh+Wizard&background=random`;
 
   if (variant === "dashboard") {
@@ -67,23 +71,50 @@ export const Header = ({ variant = "landing", user }) => {
           E-Wallet
         </h1>
       </Link>
-
-      <div className="flex items-center gap-4">
-        <Link to="/login">
-          <Button
-            variant="outline-white"
-            size="md"
-            className="hidden sm:flex rounded-[5px]"
-          >
-            SignIn
-          </Button>
-        </Link>
-        <Link to="/register">
-          <Button variant="white" size="md" className="rounded-[5px]">
-            Sign Up
-          </Button>
-        </Link>
-      </div>
+      {data !== null ? (
+        <div className="flex items-center gap-6 md:gap-8">
+          <div className="flex items-center gap-4 cursor-pointer group">
+            <div className="hidden md:block text-right">
+              <p className="text-base font-normal text-black group-hover:text-primary transition-colors">
+                {profileName}
+              </p>
+            </div>
+            <div className="flex items-center gap-2">
+              <button
+                className="cursor-pointer"
+                onClick={() => {
+                  clearSession();
+                  setIsLogin(false);
+                }}
+              >
+                <img
+                  src={profilePic}
+                  alt="Profile"
+                  className="w-11 h-11 rounded-full object-cover border border-gray-200"
+                />
+              </button>
+              <ChevronDown className="w-5 h-5 text-black group-hover:text-primary transition-colors" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <Link to="/login">
+            <Button
+              variant="outline-white"
+              size="md"
+              className="hidden sm:flex rounded-[5px]"
+            >
+              SignIn
+            </Button>
+          </Link>
+          <Link to="/register">
+            <Button variant="white" size="md" className="rounded-[5px]">
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
