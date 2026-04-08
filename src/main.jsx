e153d40b8eb229dec6456fe2752d4f17301e1e02
l1mus <alinesezcore@gmail.com>
2026-04-08 11:47:09 +0700

    feat : import initializeDB for initialaize data

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./globals.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import { initializeDB } from "./data/mockDatabase.js";
+
+initializeDB();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
