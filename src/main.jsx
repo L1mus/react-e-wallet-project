dccc578eaeaf1987c6724983a2e17df7e86c2a02
l1mus <alinesezcore@gmail.com>
2026-04-14 15:13:45 +0700

    refactor : syntax error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 // import { initializeDB } from "./data/mockDatabase.js";
-import { store } from "./redux/store.js";
+import store from "./redux/store.js";
 import { Provider } from "react-redux";
 
 // initializeDB();
